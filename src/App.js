27f3a8ad06a0e14c3345241348901325c2b4531f
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2018-10-16 17:23:34 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Table from './Table';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello React!</h1>
+      <div className="container">
+        <Table />
       </div>
     );
   }
