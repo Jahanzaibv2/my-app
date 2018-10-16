c99489e587251d3ec9a2afc8dbf0ff4988e68ae1
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2018-10-16 17:40:05 +0500

    .

---
+++ src/components/counter.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    React.createElement("div");
+    return (
+      <div>
+        <h1>Hello React</h1>
+        <button>Increment</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
